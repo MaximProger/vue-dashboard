9b8383404f0c3ef673ffa18080b51045846626da
Maksim Koshman <maksimkoshman090@gmail.com>
2020-06-22 08:58:08 +0300

    build version

---
+++ dist/precache-manifest.6ff65d3a948123a2da35a1f98ea3d52f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1db6d1584ed41b329ea",
+    "url": "/vue-dashboard/dist/css/app.0d759cc3.css"
+  },
+  {
+    "revision": "79e54828fb5340031417",
+    "url": "/vue-dashboard/dist/css/chunk-5494170a.cc460f09.css"
+  },
+  {
+    "revision": "da86d061a248182a4d7c",
+    "url": "/vue-dashboard/dist/css/chunk-675bb388.59e12682.css"
+  },
+  {
+    "revision": "228c217cbdcf58d7f3e2",
+    "url": "/vue-dashboard/dist/css/chunk-d6a33f22.be4f905e.css"
+  },
+  {
+    "revision": "e511f2e45366a90f31b924a6bc218e18",
+    "url": "/vue-dashboard/dist/index.html"
+  },
+  {
+    "revision": "d1db6d1584ed41b329ea",
+    "url": "/vue-dashboard/dist/js/app.c0a29ab5.js"
+  },
+  {
+    "revision": "79e54828fb5340031417",
+    "url": "/vue-dashboard/dist/js/chunk-5494170a.438994de.js"
+  },
+  {
+    "revision": "da86d061a248182a4d7c",
+    "url": "/vue-dashboard/dist/js/chunk-675bb388.e7dcd28a.js"
+  },
+  {
+    "revision": "228c217cbdcf58d7f3e2",
+    "url": "/vue-dashboard/dist/js/chunk-d6a33f22.a41ef371.js"
+  },
+  {
+    "revision": "6a2b8864b0273ea52ea5",
+    "url": "/vue-dashboard/dist/js/chunk-vendors.ca3d240d.js"
+  },
+  {
+    "revision": "8a5909938cfeebdbf93fc5c6c52c8f60",
+    "url": "/vue-dashboard/dist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-dashboard/dist/robots.txt"
+  }
+]);
